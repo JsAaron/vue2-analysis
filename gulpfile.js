ed47c460a27c254d4df2d763def703717a6b24ce
JsAaron <401545553@qq.com>
2016-03-20 21:54:28 +0800

    配置webpack+

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-var gulp    = require('gulp');
+var gulp = require('gulp');
 var webpack = require('webpack');
-var build   = require('./build.js')
+var build = require('./build.js')
 
 //http://www.browsersync.cn/docs/recipes/
 var browserSync = require('browser-sync').create();
@@ -30,10 +30,12 @@ function handleErrors() {
     this.emit('end');
 };
 
+//============
+// rollup部分
+//============
 
 // web服务 Server + watching scss/html files
 gulp.task('web-server', function() {
-    build()
     browserSync.init(webServer);
 });
 
@@ -47,3 +49,68 @@ gulp.task('watch', ['web-server'], function() {
 })
 
 gulp.task('default', ['watch'])
+
+
+
+//==================
+//  webpack部分
+//==================
+
+
+var miniRoot = './mini-vue'
+
+/**
+ * webpack打包es6
+ * @return {[type]} [description]
+ */
+gulp.task('webpack-pack', function() {
+    webpack({
+        // watch: true,
+        //页面入口
+        entry: miniRoot + '/src/index.js',
+        //出口文件输出配置
+        output: {
+            path: miniRoot, //js位置
+            filename: 'build.js'
+        },
+        //source map 支持
+        devtool: '#source-map',
+        //加载器
+        module: {
+            loaders: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }]
+        }
+    }, function(err, stats) {
+        if (err) {
+            handleErrors();
+        }
+    });
+})
+
+
+gulp.task('mini-server', function() {
+    browserSync.init({
+        server    : miniRoot,
+        index     : 'index.html',
+        port      : 3000,
+        logLevel  : "debug",
+        logPrefix : "Aaron",
+        open      : true,
+        files     : [miniRoot + "/*.js", miniRoot+"/index.html"] //监控变化
+    });
+})
+
+/**
+ * 迷你vue
+ */
+gulp.task('mini', ['webpack-pack','mini-server'], function() {
+    gulp.watch(miniRoot + '/src/**/*.js', function() {
+        gulp.run('webpack-pack');
+    })
+})
---
+++ mini-vue/src/instance/Ue.js
@@ -0,0 +1,12 @@
+/**
+ * Ue构造器
+ * @param {[type]} options [description]
+ */
+function Ue(options) {
+    this._init(options)
+}
+
+
+Vue.prototype._init = function(options) {
+
+}
