c0dac6d567e364b619485b04e6c17f778e0e5b91
JsAaron <401545553@qq.com>
2016-03-20 22:16:02 +0800

    update webpack

---
+++ mini-vue/src/index.js
@@ -0,0 +1,1 @@
+import Ue from './instance/Ue';
--- mini-vue/src/instance/Ue.js
+++ mini-vue/src/instance/Ue.js
@@ -7,6 +7,31 @@ function Ue(options) {
 }
 
 
-Vue.prototype._init = function(options) {
 
+Vue.prototype._init = function(options) {
+    options = options || {}
+    this.$el = null;
+    //初始化空数据
+    //通过_initScope方法填充
+    this._data = {}
+
+    this._initState()
 }
+
+
+Object.defineProperty(Vue.prototype, '$data', {
+    get: function() { 
+        return this._data
+    },
+    set: function(newData) {
+        if (newData !== this._data) {
+            this._setData(newData)
+        }
+    }
+})
+
+
+
+
+export default Ue
+ 
