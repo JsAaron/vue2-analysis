c530033e00c7165fd33f5b32f2f48803ec47267d
JsAaron <401545553@qq.com>
2016-04-04 12:32:48 +0800

    observe array

---
+++ demo-learn/demo7-观察数组/src/directives/public/index.js
@@ -0,0 +1,11 @@
+/**
+ * ЦёБо
+ */
+import on from './on'
+import text from './text'
+import model from './model/index'
+export default {
+    on,
+    model,
+    text
+}
--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('server', function() {
         port: 3000,
       //  logLevel: "debug",
       //  logPrefix: "Aaron",
-        open:false,
+        open:true,
         files: [root + "/build.js", root + "/index.html"] //监控变化
     });
 })
