587c7158029df7e36dcff39be4f6155374c9de26
jsAaron <401545553@qq.com>
2016-04-27 16:39:47 +0800

    test demo

--- vue/examples/commits/test.js
+++ vue/examples/commits/test.js
@@ -1,14 +1,41 @@
+var apiURL = 'https://api.github.com/repos/vuejs/vue/commits?per_page=3&sha='
+var isPhantom = navigator.userAgent.indexOf('PhantomJS') > -1
+
 new Vue({
 
     el: '#demo',
 
     data: {
-        checked: false
+        branches: ['master', 'dev'],
+        currentBranch: 'master',
+        commits: null
+    },
+
+    created: function() {
+        this.fetchData()
     },
 
     watch: {
-        checked: function(v) {
-            console.log(v)
+        currentBranch: 'fetchData'
+    },
+
+    methods: {
+        fetchData: function() {
+            // CasperJS fails at cross-domain XHR even with
+            // --web-security=no, have to mock data here.
+            if (isPhantom) {
+                return mockData.call(this)
+            }
+            var xhr = new XMLHttpRequest()
+            var self = this
+            xhr.open('GET', apiURL + self.currentBranch)
+            xhr.onload = function() {
+                self.commits = JSON.parse(xhr.responseText)
+                console.log(self.commits[0].html_url)
+            }
+            xhr.send()
         }
     }
+
+
 })
