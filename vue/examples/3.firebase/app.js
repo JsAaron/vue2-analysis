e31800c5f6cc152497327423a29203c5437871f0
aaron <401545553@qq.com>
2016-08-19 16:35:50 +0800

    update firebase

--- vue/examples/3.firebase/app.js
+++ vue/examples/3.firebase/app.js
@@ -5,46 +5,46 @@ var usersRef = new Firebase('https://vue-demo.firebaseIO.com/users')
 
 // create Vue app
 var app = new Vue({
-  // element to mount to
-  el: '#app',
-  // initial data
-  data: {
-    newUser: {
-      name: '',
-      email: ''
-    }
-  },
-  // firebase binding
-  // https://github.com/vuejs/vuefire
-  firebase: {
-    users: usersRef
-  },
-  // computed property for form validation state
-  computed: {
-    validation: function () {
-      return {
-        name: !!this.newUser.name.trim(),
-        email: emailRE.test(this.newUser.email)
-      }
+    // element to mount to
+    el: '#app',
+    // initial data
+    data: {
+        newUser: {
+            name: '',
+            email: ''
+        }
     },
-    isValid: function () {
-      var validation = this.validation
-      return Object.keys(validation).every(function (key) {
-        return validation[key]
-      })
-    }
-  },
-  // methods
-  methods: {
-    addUser: function () {
-      if (this.isValid) {
-        usersRef.push(this.newUser)
-        this.newUser.name = ''
-        this.newUser.email = ''
-      }
+    // firebase binding
+    // https://github.com/vuejs/vuefire
+    firebase: {
+        users: usersRef
     },
-    removeUser: function (user) {
-      usersRef.child(user['.key']).remove()
+    // computed property for form validation state
+    computed: {
+        validation: function() {
+            return {
+                name: !!this.newUser.name.trim(),
+                email: emailRE.test(this.newUser.email)
+            }
+        },
+        isValid: function() {
+            var validation = this.validation
+            return Object.keys(validation).every(function(key) {
+                return validation[key]
+            })
+        }
+    },
+    // methods
+    methods: {
+        addUser: function() {
+            if (this.isValid) {
+                usersRef.push(this.newUser)
+                this.newUser.name = ''
+                this.newUser.email = ''
+            }
+        },
+        removeUser: function(user) {
+            usersRef.child(user['.key']).remove()
+        }
     }
-  }
 })
