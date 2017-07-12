e2a666680cc6d98448829ead030f1c45e12a9420
jsAaron <401545553@qq.com>
2017-07-12 22:04:28 +0800

    add demo.html

--- vue2/examples/6.svg/svg.js
+++ vue2/examples/6.svg/svg.js
@@ -14,9 +14,9 @@ Vue.component('polygraph', {
   template: '#polygraph-template',
   computed: {
     // a computed property for the polygon's points
-    points: function () {
+    points: function() {
       var total = this.stats.length
-      return this.stats.map(function (stat, i) {
+      return this.stats.map(function(stat, i) {
         var point = valueToPoint(stat.value, i, total)
         return point.x + ',' + point.y
       }).join(' ')
@@ -32,9 +32,8 @@ Vue.component('polygraph', {
       },
       template: '#axis-label-template',
       computed: {
-        point: function () {
-          return valueToPoint(
-            +this.stat.value + 10,
+        point: function() {
+          return valueToPoint(+this.stat.value + 10,
             this.index,
             this.total
           )
@@ -45,14 +44,14 @@ Vue.component('polygraph', {
 })
 
 // math helper...
-function valueToPoint (value, index, total) {
-  var x     = 0
-  var y     = -value * 0.8
+function valueToPoint(value, index, total) {
+  var x = 0
+  var y = -value * 0.8
   var angle = Math.PI * 2 / total * index
-  var cos   = Math.cos(angle)
-  var sin   = Math.sin(angle)
-  var tx    = x * cos - y * sin + 100
-  var ty    = x * sin + y * cos + 100
+  var cos = Math.cos(angle)
+  var sin = Math.sin(angle)
+  var tx = x * cos - y * sin + 100
+  var ty = x * sin + y * cos + 100
   return {
     x: tx,
     y: ty
@@ -67,7 +66,7 @@ new Vue({
     stats: stats
   },
   methods: {
-    add: function (e) {
+    add: function(e) {
       e.preventDefault()
       if (!this.newLabel) return
       this.stats.push({
@@ -76,7 +75,7 @@ new Vue({
       })
       this.newLabel = ''
     },
-    remove: function (stat) {
+    remove: function(stat) {
       if (this.stats.length > 3) {
         this.stats.splice(this.stats.indexOf(stat), 1)
       } else {
