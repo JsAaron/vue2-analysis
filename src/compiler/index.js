3dd7e3c736e77ebe3773e42ac938cbdab8c83e02
aaron <401545553@qq.com>
2017-08-02 17:00:32 +0800

    no message

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,19 +1,45 @@
-import { parse } from './parser/index'
-import { createCompilerCreator } from './create-compiler'
+import {
+	parse
+} from './parser/index'
+import {
+	createCompilerCreator
+} from './create-compiler'
 
 /**
  * compileToFunctions => compile => baseCompile
  */
 function baseCompile(template, options) {
-  //生成语法解析树
-  const ast = parse(template.trim(), options)
-  // optimize(ast, options)
-  // const code = generate(ast, options)
-  // return {
-  //   ast,
-  //   render: code.render,
-  //   staticRenderFns: code.staticRenderFns
-  // }
+	//生成语法解析树
+	const ast = parse(template.trim(), options)
+		// optimize(ast, options)
+		// const code = generate(ast, options)
+		// return {
+		//   ast,
+		//   render: code.render,
+		//   staticRenderFns: code.staticRenderFns
+		// }
 }
 
 export const createCompiler = createCompilerCreator(baseCompile)
+
+
+(function() {
+	with(this) {
+		return _c('div', {
+			attrs: {
+				"id": "editor"
+			}
+		}, [_c('p', [_v(_s(answer))]), _v(" "), _c('textarea', {
+			domProps: {
+				"value": input
+			},
+			on: {
+				"input": update
+			}
+		}), _v(" "), _c('div', {
+			domProps: {
+				"innerHTML": _s(compiledMarkdown)
+			}
+		})])
+	}
+})
--- src/core/index.js
+++ src/core/index.js
@@ -1,9 +1,35 @@
 import Mue from './instance/index'
-import { initGlobalAPI } from './global-api/index'
+import {
+  initGlobalAPI
+} from './global-api/index'
 // import { isServerRendering } from 'core/util/env'
 
 initGlobalAPI(Mue)
 
 export default Mue
 
+/**
 
+div => attrs => id.editor
+	p => v(_s(answer))
+	_v(" ")
+	textarea => domProps=> value:input
+			 => on => input:update
+	_v(" ")
+	div => domProps => innerHTML:_s(compiledMarkdown)
+
+
+	with(this) {
+		return _c('div', {
+			attrs: { "id": "editor" }
+		}, [
+			_c('p', [_v(_s(answer))]),
+			_v(" "),
+			_c('textarea', {
+				domProps: {"value": input},
+				on: {"input": update}}),
+			_v(" "),
+			_c('div', {domProps: {"innerHTML": _s(compiledMarkdown)}
+		})])
+	}
+=
